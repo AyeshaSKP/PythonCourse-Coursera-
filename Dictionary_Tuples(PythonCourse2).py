4711bfddfa1db8f3de4e77628de9e7c8ff6dad31
Ayesha <41141945+AyeshaSKP@users.noreply.github.com>
2024-02-12 04:08:08 +0000

    Update Dictionary_Tuples(PythonCourse2).py

--- Dictionary_Tuples(PythonCourse2).py
+++ Dictionary_Tuples(PythonCourse2).py
@@ -165,3 +165,73 @@ low_d = {}
 for c in p.lower():
     low_d[c]=low_d.get(c,0)+1
 print(low_d)
+
+#Tuple
+#Create a tuple called practice that has four elements: ‘y’, ‘h’, ‘z’, and ‘x’.
+practice = 'y','h','z','x'
+print(practice)
+
+# Provided is a list of tuples. Create another list called t_check that contains the third element of every tuple.
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+t_check=[]
+for element in lst_tups:
+    t_check.append(element[2])
+    
+print(t_check)
+
+#Below, we have provided a list of tuples. Write a for loop that saves the second element of each tuple into a list called seconds.
+tups = [('a', 'b', 'c'), (8, 7, 6, 5), ('blue', 'green', 'yellow', 'orange', 'red'), (5.6, 9.99, 2.5, 8.2), ('squirrel', 'chipmunk')]
+
+seconds=[]
+
+for element in tups:
+    seconds.append(element[1])
+    
+print(seconds)
+
+fruits = ['apple', 'pear', 'apricot', 'cherry', 'peach']
+for i in fruits:
+    print(len(i),i)
+   
+print(i,fruits.index(i))
+
+#The pythonic way to consume the results of enumerate, however, is to unpack the tuples while iterating through them, so that the code is easier to understand.
+for idx, fruit in enumerate(fruits):
+    print(idx, fruit)
+
+#With only one line of code, assign the variables water, fire, electric, and grass to the values “Squirtle”, “Charmander”, “Pikachu”, and “Bulbasaur”
+(water, fire, electric, grass) = ('Squirtle', 'Charmander', 'Pikachu', 'Bulbasaur')   
+
+#If you remember, the .items() dictionary method produces a sequence of tuples. Keeping this in mind, we have provided you a dictionary called pokemon. For every key value pair, append the key to the list p_names, and append the value to the list p_number. Do not use the .keys() or .values() methods.
+pokemon = {'Rattata': 19, 'Machop': 66, 'Seel': 86, 'Volbeat': 86, 'Solrock': 126}
+p_names=[]
+p_number=[]
+
+for k,v in pokemon.items():
+    p_names.append(k)
+    p_number.append(v)
+
+#Create a tuple called olympics with four elements: “Beijing”, “London”, “Rio”, “Tokyo”.
+olympics = ('Beijing', 'London', 'Rio', 'Tokyo')
+
+#    The list below, tuples_lst, is a list of tuples. Create a list of the second elements of each tuple and assign this list to the variable country.
+tuples_lst = [('Beijing', 'China', 2008), ('London', 'England', 2012), ('Rio', 'Brazil', 2016, 'Current'), ('Tokyo', 'Japan', 2020, 'Future')]
+country =[]
+for element in tuples_lst:
+    country.append(element[1])
+print(country)
+#Define a function called info with five parameters: name, gender, age, bday_month, and hometown. The function should then return a tuple with all five parameters in that order.
+def info(country, gender, age, bday_month,hometown):
+    return (country, gender, age, bday_month,hometown)
+
+print(info("Syria","male",90,"May","N/A"))
+
+#Given is the dictionary, gold, which shows the country and the number of gold medals they have earned so far in the 2016 Olympics. Create a list, num_medals, that contains only the number of medals for each country. You must use the .items() method. Note: The .items() method provides a list of tuples. Do not use .keys() method.
+gold = {'USA':31, 'Great Britain':19, 'China':19, 'Germany':13, 'Russia':12, 'Japan':10, 'France':8, 'Italy':8}
+num_medals=[]
+for k,v in gold.items():
+    num_medals.append(v)
+
+print(num_medals)
